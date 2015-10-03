2b64e53e5ebe868067707d543293f429e19991ac
Christian Noreña <crisdanovagoodyear@hotmail.com>
2015-10-03 01:52:37 -0500

    multimedia.js editado

--- multimedia.js
+++ multimedia.js
@@ -1,1 +1,17 @@
+window.onload = function(){
+	content = document.getElementById('content');
+}
 
+var tvshows = ["Breaking bad", "The Big Bang theory", "Walking dead", "Friends", "Grey's anatomy", "Game of thrones", "How I met your mother", "Modern Family"];
+var anime = [];
+var classics = [];
+var documentaries = [];
+var drama = [];
+var horror = [];
+var romance = [];
+
+function printElementsByCathegory( cathegory ){
+	for(var i = 0; i < cathegory.length; i++){
+		content.value = cathegory[i];
+	}
+}
